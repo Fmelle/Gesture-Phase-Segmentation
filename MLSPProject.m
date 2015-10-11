6a0b3628a4f0937266ac52a6e466548dce19b2a3
Paul Buchana <buchana.work@gmail.com>
2015-10-11 18:26:52 +0200

    Renamed Folder Added Matlab code

---
+++ MLSPProject.m
@@ -0,0 +1,24 @@
+%{
+Author(s): Omar Syed, Fridtjof Melle & Paul Buchana
+E-Mail(s): osyed@andrew.cmu.edu, fmelle@andrew.cmu.edu & 
+% pbuchana@andrew.cmu.edu
+Last Modified: 11/28/2015
+%}
+
+tic
+
+% Clearing active console, closing all figures and removing historical list
+% of commands.
+clear ALL; close ALL; clc;
+
+% Creating a structure to store the Processed Data files.
+fileContainer = struct([]);
+srcFiles = dir('data_files\*.csv');
+for i = 1 : length(srcFiles)
+    filename = strcat('data_files\',srcFiles(i).name);
+    fileData = xlsread(filename);
+    fileContainer{i} = fileData;
+end
+
+computationTime = toc;
+fprintf('Computaion Time: %f \n', computationTime);
